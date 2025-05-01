146c9dddedc28c0b8d9005a9c811ded9250145d2
nylespham <nylespham@gmail.com>
2025-05-01 17:36:19 +0700

    ASD-0 tf fmt

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ provider "helm" {
 }
 
 locals {
-  k8s_connector_id = replace(var.harness_delegate_name, "-", "_")
-  harness_env_id = replace(var.harness_delegate_name, "-", "_")
+  k8s_connector_id          = replace(var.harness_delegate_name, "-", "_")
+  harness_env_id            = replace(var.harness_delegate_name, "-", "_")
   harness_infrastructure_id = replace(var.harness_delegate_name, "-", "_")
 }
 module "delegate" {
